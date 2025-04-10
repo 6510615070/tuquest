bb16022299a2501e1a5c153d0ed1821678be2f38
6510615138 <r.nakinton@gmail.com>
2025-04-10 14:25:17 +0700

    add validator.dart

---
+++ lib/material/validator.dart
@@ -0,0 +1,30 @@
+class Validator {
+  static String? username(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Cannot be empty';
+    }
+
+    return null;
+  }
+
+  static String? email(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    }
+    final emailRegex = RegExp(
+      r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+    );
+    if (!emailRegex.hasMatch(value)) {
+      return 'Enter a valid email';
+    }
+    return null;
+  }
+
+  static String? password(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Cannot be empty';
+    }
+
+    return null;
+  }
+}
