032983567ef230721fb039f8570890d4e98afbf5
dkanchn <6510615013@cn330>
2025-04-19 03:59:47 +0700

    finished login file

--- lib/screens_v2/login.dart
+++ lib/screens_v2/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'home.dart';
+import './home/home.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
