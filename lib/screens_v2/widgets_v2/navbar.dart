1bffa598aa0778213dc8c8c170854c7fa277ddcb
dkanchn <6510615013@cn330>
2025-04-20 01:30:37 +0700

    fix error navbar

--- lib/screens_v2/widgets_v2/navbar.dart
+++ lib/screens_v2/widgets_v2/navbar.dart
@@ -1,9 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/navbar_provider.dart';
 import '../fav.dart';
 import '../account.dart';
 import '../home/home.dart';
 
 class CustomNavBar extends StatelessWidget {
+  const CustomNavBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final navBarProvider = Provider.of<NavBarProvider>(context);
+
+    return BottomNavigationBar(
+      currentIndex: navBarProvider.currentIndex,
+      onTap: (index) => _onItemTapped(context, index),
+      selectedItemColor: const Color(0xFFFF9D00),
+      unselectedItemColor: Colors.grey,
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite_border),
+          label: 'Favorite',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.account_circle_outlined),
+          label: 'Account',
+        ),
+      ],
+    );
+  }
+
+  void _onItemTapped(BuildContext context, int index) {
+    final navBarProvider = Provider.of<NavBarProvider>(context, listen: false);
+    navBarProvider.changeIndex(index);
+
+    final routes = <Map<String, dynamic>>[
+      {'page': const FavPage(), 'name': '/fav'},
+      {'page': const HomePage(), 'name': '/home'},
+      {'page': const AccountPage(), 'name': '/account'},
+    ];
+
+    if (ModalRoute.of(context)?.settings.name != routes[index]['name']) {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (_) => routes[index]['page'] as Widget,
+          settings: RouteSettings(name: routes[index]['name'] as String),
+        ),
+        (route) {
+          if (index == 1) return route.isFirst;
+          return route.settings.name == '/home' || route.isFirst;
+        },
+      );
+    }
+  }
+}
+/*class CustomNavBar extends StatelessWidget {
   final int selectedIndex;
 
   const CustomNavBar({
@@ -20,15 +76,6 @@ class CustomNavBar extends StatelessWidget {
       {'page': const AccountPage(), 'name': '/account'},
     ];
 
-    /*Navigator.pushAndRemoveUntil(
-      context,
-      MaterialPageRoute(
-        builder: (_) => routes[index]['page'] as Widget,
-        settings: RouteSettings(name: routes[index]['name'] as String),
-      ),
-      (route) => route.settings.name == '/home' || route.isFirst,
-    );
-  }*/
     // ตรวจสอบว่าหน้าปัจจุบันเป็นหน้าที่ต้องการเปิดหรือไม่
   if (ModalRoute.of(context)?.settings.name != routes[index]['name']) {
     Navigator.pushAndRemoveUntil(
@@ -70,4 +117,4 @@ class CustomNavBar extends StatelessWidget {
       ],
     );
   }
-}
+}*/
