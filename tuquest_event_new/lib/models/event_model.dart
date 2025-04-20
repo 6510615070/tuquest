5f10ef8d9aea3626911de3de131d51bc457bab0b
Napat Wongkaew <6510615070@cn330>
2025-04-20 22:14:01 +0700

    latest work 1

--- tuquest_event_new/lib/models/event_model.dart
+++ tuquest_event_new/lib/models/event_model.dart
@@ -1,6 +1,40 @@
 class EventModel {
-  final DateTime date;
+  final String id;
   final String title;
+  final String description;
+  final DateTime start;
+  final DateTime end;
+  final String? imageUrl;
+  final String createdBy;
 
-  EventModel({required this.date, required this.title});
+  EventModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.start,
+    required this.end,
+    this.imageUrl,
+    required this.createdBy, required DateTime date,
+  });
+
+  Map<String, dynamic> toMap() => {
+    'title': title,
+    'description': description,
+    'start': start.toIso8601String(),
+    'end': end.toIso8601String(),
+    'imageUrl': imageUrl,
+    'createdBy': createdBy,
+  };
+
+  factory EventModel.fromMap(String id, Map<String, dynamic> data) {
+    return EventModel(
+      id: id,
+      title: data['title'],
+      description: data['description'],
+      start: DateTime.parse(data['start']),
+      end: DateTime.parse(data['end']),
+      imageUrl: data['imageUrl'],
+      createdBy: data['createdBy'],
+    );
+  }
 }
